da14f9c51668c6656308bc8f776b354957dcae4a
peng <1143038749@qq.com>
2019-11-22 19:36:56 +0800

    0.1.6 version

--- src/main/java/com/peng/SpringbootOneApplication.java
+++ src/main/java/com/peng/SpringbootOneApplication.java
@@ -2,12 +2,20 @@ package com.peng;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootOneApplication {
+public class SpringbootOneApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootOneApplication.class, args);
+    }
+
+    @Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
     }
 
 }
