d4f961858aaeb7923211a5486d2804e0d293291a
lurenha <1143038749@qq.com>
2020-03-21 23:35:25 +0800

    version ---重构Service

--- src/main/java/com/peng/controller/Admin/BlogController.java
+++ src/main/java/com/peng/controller/Admin/BlogController.java
@@ -1,51 +1,53 @@
-//package com.peng.controller.Admin;
-//
-//import com.github.pagehelper.PageInfo;
-//import com.peng.domain.Blog;
-//import com.peng.domain.JsonResult.JsonResult;
-//import com.peng.service.IBlogService;
-//import org.apache.shiro.authz.annotation.Logical;
-//import org.apache.shiro.authz.annotation.RequiresPermissions;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//
-//@RestController
-//@RequestMapping("/admin/blog")
-//public class BlogController {
-//    @Autowired
-//    private IBlogService blogService;
-//
-//    @RequiresPermissions("blog:addORedit")
-//    @RequestMapping(path = "/addORedit", method = RequestMethod.POST)
-//    public JsonResult addORedit_blog(Blog blog) {
-//        boolean bool = blogService.addORedit(blog);
-//        if (bool) {
-//            return new JsonResult(20000, "提交成功!", bool);
-//        } else {
-//            return new JsonResult(50001, "提交失败!", bool);
-//        }
-//
-//    }
-//
-//    @RequiresPermissions("blog:delete")
-//    @RequestMapping(path = "/delete/{idNum}", method = RequestMethod.POST)
-//    public JsonResult delete_blog(@PathVariable("idNum") Integer bl_id) {
-//        boolean bool = blogService.deleteByid(bl_id);
-//        if (bool) {
-//            return new JsonResult(20000, "提交成功!", bool);
-//        } else {
-//            return new JsonResult(50001, "提交失败!", bool);
-//        }
-//    }
-//
-//    @RequiresPermissions("blog:find")
-//    @RequestMapping(path = "/find/{idNum}", method = RequestMethod.POST)
-//    public JsonResult find_blog(@PathVariable("idNum") Integer bl_id) {
-//        Blog blog = blogService.findByid(bl_id);
-//        return new JsonResult(20000, "ok", blog);
-//    }
-//
+package com.peng.controller.Admin;
+
+
+import com.peng.entity.Blog;
+import com.peng.entity.Result.JsonResult;
+import com.peng.entity.Result.ResultCode;
+import com.peng.entity.Result.ResultUtil;
+import com.peng.service.IBlogService;
+import org.apache.shiro.authz.annotation.Logical;
+import org.apache.shiro.authz.annotation.RequiresPermissions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/admin/blog")
+public class BlogController {
+    @Autowired
+    private IBlogService blogService;
+
+    @RequiresPermissions("blog:addORedit")
+    @RequestMapping(path = "/addORedit", method = RequestMethod.POST)
+    public JsonResult saveOrUpdate(Blog blog) {
+        boolean bool = blogService.saveOrUpdate(blog);
+        if (bool) {
+            return ResultUtil.successNoData(ResultCode.SUCCESS);
+        } else {
+            return ResultUtil.faile(ResultCode.DATA_IS_WRONG);
+        }
+
+    }
+
+    @RequiresPermissions("blog:delete")
+    @RequestMapping(path = "/delete/{idNum}", method = RequestMethod.POST)
+    public JsonResult removeById(@PathVariable("idNum") Integer blId) {
+        boolean bool = blogService.removeById(blId);
+        if (bool) {
+            return ResultUtil.successNoData(ResultCode.SUCCESS);
+        } else {
+            return ResultUtil.faile(ResultCode.DATA_IS_WRONG);
+        }
+    }
+
+    @RequiresPermissions("blog:find")
+    @RequestMapping(path = "/find/{idNum}", method = RequestMethod.POST)
+    public JsonResult getById(@PathVariable("idNum") Integer blId) {
+        Blog blog = blogService.getById(blId);
+        return ResultUtil.success(blog,ResultCode.SUCCESS);
+    }
+
 //    @RequiresPermissions(logical = Logical.AND, value = {"blog:list"})
 //    @RequestMapping("/list")
 //    public JsonResult list_blog(@RequestParam(value = "page", defaultValue = "1") Integer pageNum,
@@ -53,6 +55,7 @@
 //                                @RequestParam(value = "title", required = false) String title,
 //                                @RequestParam(value = "type", required = false) Integer ty_id
 //    ) {
+//        blogService.
 //        PageInfo<Blog> findpage = blogService.findpage(pageNum, pagesize, title, ty_id);
 //        return new JsonResult(20000, "ok", findpage);
 //    }
@@ -68,5 +71,5 @@
 //            return new JsonResult(50001, "提交失败!", bool);
 //        }
 //    }
-//
-//}
+
+}
---
+++ src/main/java/com/peng/entity/Result/ResultCode.java
@@ -0,0 +1,61 @@
+package com.peng.entity.Result;
+
+//状态码
+public enum ResultCode {
+    /* 成功状态码 */
+    SUCCESS(0, "成功"),
+
+    /* 参数错误：10001-19999 */
+    PARAM_IS_INVALID(10001, "参数无效"),
+    PARAM_IS_BLANK(10002, "参数为空"),
+    PARAM_TYPE_BIND_ERROR(10003, "参数类型错误"),
+    PARAM_NOT_COMPLETE(10004, "参数缺失"),
+
+    /* 用户错误：20001-29999*/
+    USER_NOT_LOGGED_IN(20001, "用户未登录"),
+    USER_LOGIN_ERROR(20002, "账号不存在或验证码错误"),
+    USER_ACCOUNT_FORBIDDEN(20003, "账号已被禁用"),
+    USER_NOT_EXIST(20004, "用户不存在"),
+    USER_HAS_EXISTED(20005, "用户已存在"),
+    Token_AUTH_ERROR(20006, "Token认证失败"),
+
+    /* 业务错误：30001-39999 */
+    SPECIFIED_QUESTIONED_USER_NOT_EXIST(30001, "某业务出现问题"),
+
+    /* 系统错误：40001-49999 */
+    SYSTEM_INNER_ERROR(40001, "系统繁忙，请稍后重试"),
+
+    /* 数据错误：50001-599999 */
+    RESULE_DATA_NONE(50001, "数据未找到"),
+    DATA_IS_WRONG(50002, "数据有误"),
+    DATA_ALREADY_EXISTED(50003, "数据已存在"),
+
+    /* 接口错误：60001-69999 */
+    INTERFACE_INNER_INVOKE_ERROR(60001, "内部系统接口调用异常"),
+    INTERFACE_OUTTER_INVOKE_ERROR(60002, "外部系统接口调用异常"),
+    INTERFACE_FORBID_VISIT(60003, "该接口禁止访问"),
+    INTERFACE_ADDRESS_INVALID(60004, "接口地址无效"),
+    INTERFACE_REQUEST_TIMEOUT(60005, "接口请求超时"),
+    INTERFACE_EXCEED_LOAD(60006, "接口负载过高"),
+    INTERFACE_METHOD_ERROR(60007, "接口请求方式异常"),
+
+
+    /* 权限错误：70001-79999 */
+    PERMISSION_NO_ACCESS(70001, "无访问权限");
+
+    private Integer code;
+    private String msg;
+
+    ResultCode(Integer code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+}
